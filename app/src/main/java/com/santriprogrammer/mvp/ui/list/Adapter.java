4185ba39b28e3667944a8ea8944679eec11fd7e1
Muhammad Fikri Imaduddin Masyhuri <fikri.im@gmail.com>
2017-10-25 13:29:16 +0700

    perbaikan

--- app/src/main/java/com/santriprogrammer/mvp/ui/list/Adapter.java
+++ app/src/main/java/com/santriprogrammer/mvp/ui/list/Adapter.java
@@ -2,13 +2,9 @@ package com.santriprogrammer.mvp.ui.list;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.constraint.ConstraintLayout;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.util.Pair;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -19,8 +15,6 @@ import butterknife.ButterKnife;
 import com.bumptech.glide.Glide;
 import com.santriprogrammer.mvp.R;
 import com.santriprogrammer.mvp.model.Pojo;
-import com.santriprogrammer.mvp.model.PojoBaru.DataBean;
-import com.santriprogrammer.mvp.ui.detail.Detail;
 import com.santriprogrammer.mvp.ui.list.Adapter.ViewHolder;
 import com.santriprogrammer.mvp.ui.perkat.PerkatActivity;
 import com.santriprogrammer.mvp.utils.Helper.Movie;
